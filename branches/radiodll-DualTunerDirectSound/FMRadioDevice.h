b25ebcbeaf0c3e18a76657aea497b2b1d647b601
Pete9008 <Pete9008@710a5778-8b44-0410-8d0b-473e63153847>
2008-09-06 14:39:09 +0000

    Merged dual tuner and Direct Sound release.
    Mostly working but still something funny going on with the NewsActive export in the debug build.

---
+++ branches/radiodll-DualTunerDirectSound/FMRadioDevice.h
@@ -0,0 +1,546 @@
+// FMRadioDevice.h: interface for the CFMRadioDevice class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#if !defined(AFX_FMRADIODEVICE_H__6F0D8129_C34F_4CBC_93A0_EC3F2F68A8D1__INCLUDED_)
+#define AFX_FMRADIODEVICE_H__6F0D8129_C34F_4CBC_93A0_EC3F2F68A8D1__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif
+
+#define _WIN32_WINNT 0x0500
+
+typedef GUID *LPGUID;
+
+#include <windows.h>
+#include <streams.h>
+
+
+//ULONG/DWORD pointer types defined for DDK if not already
+#ifndef ULONG_PTR
+#define ULONG_PTR	ULONG
+#endif
+
+#ifndef DWORD_PTR
+#define DWORD_PTR	DWORD
+#endif
+
+#ifndef DDK_SERVER_2003_DDK
+//#pragma message ("Using Windows Server 2003 DDK")
+// Use the setupapi.lib file that comes with the compiler
+// (even though the DDK also has a setupapi.lib library)
+#pragma comment (lib, "setupapi.lib")
+// Use the hid.lib file from the appropriate DDK directory
+#pragma comment (lib, "hid.lib")
+#pragma comment (lib, "kernel32.lib")
+#endif
+
+#define MAX_LOADSTRING 256
+
+extern "C" {
+#include "hidsdi.h"
+}
+#include "setupapi.h"
+
+#include <dbt.h>
+
+//Multimedia header
+#include "mmsystem.h"
+
+#include "RDSData.h"
+#include "XYCriticalSection.h"
+
+//Max number of USB Devices allowed
+#define MAX_USB_DEVICES	64
+
+#define FMTUNERDATA_REPORT_SIZE	61
+
+//Device data
+#define SILABS_VID	0x10C4
+#define SILABS_PID	0x818A
+#define PCEAR_VID	0x10C5
+#define PCEAR_PID	0x819A
+#define ADSTECH_VID	0x06E1
+#define ADSTECH_PID	0xA155
+
+#define FMRADIO_SW_VERSION		7
+#define FMRADIO_HW_VERSION		1
+#define FMRADIO_FMTUNER_VERSION	10
+
+//Audio data
+#define SAMPLES_PER_SECOND	96000
+#define BITS_PER_SAMPLE		16
+#define CHANNELS			2
+
+//Our stream data should be in 20 blocks at 32KB each
+#define BLOCK_SIZE		32768
+#define BLOCK_COUNT		20
+#define BUFFER_PADDING	19
+
+//Return Codes
+#define STATUS_OK					0x00
+#define STATUS_ERROR				0x01
+#define STATUS_OUTPUTAUDIO_ERROR	0x02
+#define STATUS_FMRADIOAUDIO_ERROR	0x03
+#define STATUS_FMRADIODATA_ERROR	0x04
+#define STATUS_BOOTLOAD_ERROR		0x05
+
+//Register definitions
+#define FMRADIO_REGISTER_NUM	16
+#define RDS_REGISTER_NUM		6
+
+#define FMRADIO_REGISTER	WORD
+#define FMRADIO_REGISTER_SIZE	sizeof(FMRADIO_REGISTER)
+#define RDS_REGISTER_SIZE	sizeof(FMRADIO_REGISTER)
+
+#define DEVICEID		0
+#define CHIPID			1
+#define POWERCFG		2
+#define CHANNEL			3
+#define SYSCONFIG1		4
+#define SYSCONFIG2		5
+#define SYSCONFIG3		6
+#define TEST1			7
+#define TEST2			8
+#define BOOTCONFIG		9
+#define STATUSRSSI		10
+#define READCHAN		11
+#define RDSA			12
+#define RDSB			13
+#define RDSC			14
+#define RDSD			15
+
+//Bit definitions
+#define DEVICEID_PN		0xF000
+#define DEVICEID_MFGID	0x0FFF
+
+#define CHIPID_REV		0xFC00
+#define CHIPID_DEV		0x0200
+#define CHIPID_FIRMWARE	0x01FF
+
+#define POWERCFG_DMUTE		0x4000
+#define POWERCFG_MONO		0x2000
+#define POWERCFG_SEEKUP		0x0200
+#define POWERCFG_SEEK		0x0100
+#define POWERCFG_DISABLE	0x0040
+#define POWERCFG_ENABLE		0x0001
+
+#define CHANNEL_TUNE	0x8000
+#define CHANNEL_CHAN	0x03FF
+
+#define SYSCONFIG1_DE		0x0800
+#define SYSCONFIG1_RDS		0x1000
+
+#define SYSCONFIG2_SEEKTH	0xFF00
+#define SYSCONFIG2_BAND		0x0080
+#define SYSCONFIG2_SPACE	0x0030
+#define SYSCONFIG2_VOLUME	0x000F
+
+#define SYSCONFIG2_SPACE_200KHZ	0x0000
+#define SYSCONFIG2_SPACE_100KHZ	0x0010
+#define SYSCONFIG2_SPACE_50KHZ	0x0020
+
+#define STATUSRSSI_RDSR		0x8000
+#define STATUSRSSI_STC		0x4000
+#define STATUSRSSI_SF		0x2000
+#define STATUSRSSI_ST		0x0100
+#define STATUSRSSI_RSSI		0x00FF
+
+#define READCHAN_READCHAN	0x03FF
+
+//Scratch page
+#define SCRATCH_PAGE_SIZE		63
+#define SCRATCH_PAGE_USED_SIZE	22
+typedef BYTE	SCRATCH_PAGE[SCRATCH_PAGE_SIZE];
+
+#define SCRATCH_PAGE_SW_VERSION	1
+#define SCRATCH_PAGE_HW_VERSION	2
+
+//Report definitions
+#define DEVICEID_REPORT		DEVICEID + 1
+#define CHIPID_REPORT		CHIPID + 1
+#define POWERCFG_REPORT		POWERCFG + 1
+#define CHANNEL_REPORT		CHANNEL + 1
+#define SYSCONFIG1_REPORT	SYSCONFIG1 + 1
+#define SYSCONFIG2_REPORT	SYSCONFIG2 + 1
+#define SYSCONFIG3_REPORT	SYSCONFIG3 + 1
+#define TEST1_REPORT		TEST1 + 1
+#define TEST2_REPORT		TEST2 + 1
+#define BOOTCONFIG_REPORT	BOOTCONFIG + 1
+#define STATUSRSSI_REPORT	STATUSRSSI + 1
+#define READCHAN_REPORT		READCHAN + 1
+#define RDSA_REPORT			RDSA + 1
+#define RDSB_REPORT			RDSB + 1
+#define RDSC_REPORT			RDSC + 1
+#define RDSD_REPORT			RDSD + 1
+#define LED_REPORT			19
+#define STREAM_REPORT		19
+#define SCRATCH_REPORT		20
+
+#define ENTIRE_REPORT		FMRADIO_REGISTER_NUM + 1
+#define RDS_REPORT			FMRADIO_REGISTER_NUM + 2
+
+#define REGISTER_REPORT_SIZE	(FMRADIO_REGISTER_SIZE + 1)
+#define ENTIRE_REPORT_SIZE		((FMRADIO_REGISTER_NUM * FMRADIO_REGISTER_SIZE) + 1)
+#define RDS_REPORT_SIZE			((RDS_REGISTER_NUM * FMRADIO_REGISTER_SIZE) + 1)
+#define LED_REPORT_SIZE			3
+#define STREAM_REPORT_SIZE		3
+#define SCRATCH_REPORT_SIZE		(SCRATCH_PAGE_SIZE + 1)
+
+//LED State definitions
+#define LED_COMMAND			0x35
+
+#define NO_CHANGE_LED		0x00
+#define ALL_COLOR_LED		0x01
+#define BLINK_GREEN_LED		0x02
+#define BLINK_RED_LED		0x04
+#define BLINK_ORANGE_LED	0x10
+#define SOLID_GREEN_LED		0x20
+#define SOLID_RED_LED		0x40
+#define SOLID_ORANGE_LED	0x80
+
+#define CONNECT_STATE		BLINK_GREEN_LED
+#define DISCONNECT_STATE	BLINK_ORANGE_LED
+#define BOOTLOAD_STATE		SOLID_RED_LED
+#define STREAMING_STATE		ALL_COLOR_LED
+#define TUNING_STATE		SOLID_GREEN_LED
+#define SEEKING_STATE		SOLID_GREEN_LED
+
+//Stream State definitions
+#define STREAM_COMMAND	0x36
+
+#define STREAM_VIDPID	0x00
+#define STREAM_AUDIO	0xFF
+
+//Function definitions
+#define SEEK_UP		true
+#define SEEK_DOWN	false
+
+#define TUNE_UP		true
+#define TUNE_DOWN	false
+
+//Volume Definitions
+#define VOLUME_MIN	0		//0% - 100%
+#define VOLUME_MAX	100
+
+//Timeout definitions
+#define	POLL_TIMEOUT_SECONDS		3
+#define DISCONNECT_TIMEOUT_SECONDS	15
+
+//Seek Threshold Definitions
+#define MAX_SEEK_THRESHOLD			63
+#define PREFERRED_SEEK_THRESHOLD	25
+
+//Number of presets definition
+#define PRESET_NUM	12
+//Number of PI definition
+#define PI_NUM	30
+#define FLASH_PI_NUM	12
+
+//Global variables for the critical section and 
+//free block count, used by callback functions
+static CRITICAL_SECTION gWaveCriticalSection;
+static volatile BYTE gWaveFreeBlockCount;
+
+// convenient macro for releasing interfaces
+#define HELPER_RELEASE(x)   if (x != NULL) \
+                            { \
+                                x->Release(); \
+                                x = NULL; \
+                            }
+
+//Structure that contains all useful data about the radio - filled from the scratch page
+typedef struct RadioData
+{
+	bool firstRun;
+	bool enableDirectShow;
+	BYTE swVersion;
+	BYTE hwVersion;
+	BYTE partNumber;
+	WORD manufacturerID;
+	BYTE chipVersion;
+	BYTE deviceVersion;
+	BYTE firmwareVersion;
+	double currentStation;
+	double preset[PRESET_NUM];
+	WORD PI[PI_NUM];
+	WORD EonPI;
+	double PI_Freq[PI_NUM];
+	BYTE seekThreshold;
+	BYTE band;
+	BYTE spacing;
+	BYTE deemphasis;
+	BYTE monoStereo;
+	bool alwaysOnTop;
+	bool showInTray;
+	bool showInTitleBar;
+	bool muteOnStartup;
+	BYTE scanTime;
+	BYTE bufferSize;
+} RadioData;
+
+typedef std::map<float, float> tAFMap;
+//typedef std::pair<WORD, float> EONPI_Pair;
+//typedef std::multimap<WORD, float> tEONPIMap;
+
+// This is also added to the header for the DLL, protect from multiple inclusions
+#ifndef _RDSDATA_
+//Structure that contains the RDS data
+typedef struct RDSData
+{
+	double currentStation;
+	BYTE recievedSignalStrength;
+	bool isStereo;
+	std::string rdsText;
+	std::string rdsPS;
+	WORD rdsPI;
+	BYTE rdsPTY;
+	bool rdsTA;
+	bool rdsTP;
+	bool rdsMS;
+	bool rdsEON;
+	std::string rdsPTYString;
+	tAFMap AFMap;
+	//tEONPIMap EONPIMap;	
+	WORD mEON_TrafficPI;
+	std::string rdsPICountry;
+	std::string rdsPIRegion;
+
+} RDSData;
+#define _RDSDATA_
+#endif
+
+//Values used within the RadioData structure to assign values to the band, spacing, deemphasis, and gui options
+#define DATA_BAND				0x20
+#define DATA_BAND_875_108MHZ	0x00
+#define DATA_BAND_76_90MHZ		0x20
+
+#define DATA_SPACING			0x0C
+#define DATA_SPACING_200KHZ		0x00
+#define DATA_SPACING_100KHZ		0x04
+#define DATA_SPACING_50KHZ		0x08
+
+#define DATA_DEEMPHASIS			0x02
+#define DATA_DEEMPHASIS_75		0x00
+#define DATA_DEEMPHASIS_50		0x02
+
+#define DATA_MONOSTEREO			0x10
+#define DATA_MONOSTEREO_STEREO	0x00
+#define DATA_MONOSTEREO_MONO	0x10
+
+#define DATA_ALWAYSONTOP		0x80
+#define DATA_SHOWINTRAY			0x40
+#define DATA_SHOWINTITLEBAR		0x20
+#define DATA_MUTEONSTARTUP		0x10
+#define DATA_SCANTIME			0x0F
+
+//Advanced Options
+#define FLAG_DEDUP				0x01
+#define FLAG_100Khz				0x02
+#define FLAG_200Khz				0x04
+#define FLAG_MUTESTREAM			0x08
+
+class CFMRadioDevice  
+{
+public:
+	CFMRadioDevice(bool primary);
+	virtual ~CFMRadioDevice();
+
+private:
+	HRESULT CFMRadioDevice::LoadGraphFile(IGraphBuilder *pGraph, const WCHAR* wszName);
+	HRESULT CFMRadioDevice::SaveGraphFile(IGraphBuilder *pGraph, WCHAR *wszPath);
+	int CFMRadioDevice::InitDirectShow();
+
+//////////////////////
+//General Functionality
+
+public:
+	bool    change_process_priority;
+	DWORD   m_previous_process_priority;
+	bool    m_process_priority_set;
+	long	ExFlags;
+
+	BYTE	OpenFMRadio(RadioData* radioData);
+	bool	CloseFMRadio();
+
+//////////////////////
+//////////////////////
+
+///////////////////////
+//USB HID Functionality
+
+public:
+	bool	BootloadDevice(RadioData* radioData);
+	
+	bool	StopStream(bool stop);
+	bool	Mute(bool mute);
+	bool	Tune(bool tuneUp);
+	bool	Tune(double frequency);
+	bool	PITune(double frequency, int targetPI, double PIFrequency);
+	bool	DoTune(double frequency);
+	bool	DoQuickTune(double frequency);
+	bool	Seek(bool seekUp);
+	bool	QuickSeek(bool seekUp);
+	bool	GetRDSData(RDSData* radioData);
+	WORD	GetRDSPI(void);
+	BYTE	GetRDSPTY(void);
+	WORD	GetEONNewsPI(bool Clear);
+	bool	RTAStart (char windowName[256], short dwData, char lpData[256]);
+	bool	RTAStop (char windowName[256], short dwData, char lpData[256]);
+	bool	RNewsStart (char windowName[256], short dwData, char lpData[256]);
+	bool	RNewsStop (char windowName[256], short dwData, char lpData[256]);
+	bool	RRadioText (char windowName[256], short dwData, char lpData[256]);
+
+	//bool	updateRDSData(RDSData* radioData);
+	void	updateRDSData();
+	void	ResetRDSText();
+	bool	SaveRadioSettings(RadioData* radioData);	
+	bool	WriteRegister(BYTE report, FMRADIO_REGISTER registers);
+	bool	ReadAllRegisters(FMRADIO_REGISTER *registers);
+	bool	UpdateRDS();
+
+	bool	SetRegisterReport(BYTE report, FMRADIO_REGISTER* dataBuffer, DWORD dataBufferSize);
+	bool	GetRegisterReport(BYTE report, FMRADIO_REGISTER* dataBuffer, DWORD dataBufferSize);
+
+	bool CFMRadioDevice::Send_TA_Start(void) {return(m_RDS.Send_TA_Start());};
+	bool CFMRadioDevice::Send_TA_Stop(void) {return(m_RDS.Send_TA_Stop());};
+	bool CFMRadioDevice::Send_News_Start(void) {return(m_RDS.Send_News_Start());};
+	bool CFMRadioDevice::Send_News_Stop(void) {return(m_RDS.Send_News_Stop());};
+	
+	//bool	SetLED(BYTE ledState) {return(ChangeLED(ledState));};
+
+private:
+	bool	OpenFMRadioData();
+	bool	GetRadioData(RadioData* radioData);
+	bool	SetRadioData(RadioData* radioData);
+	bool	InitializeRadioData(RadioData* radioData);
+	bool	CloseFMRadioData();
+	RDSData lrdsData;
+
+	CRITICAL_SECTION m_RDSCriticalSection;
+	
+	HANDLE	m_FMRadioDataHandle;
+	HANDLE	m_FMRadioRDSHandle;
+
+	BYTE*	m_pEndpoint0ReportBuffer;
+	DWORD	m_Endpoint0ReportBufferSize;
+
+	BYTE*	m_pEndpoint1ReportBuffer;
+	DWORD	m_Endpoint1ReportBufferSize;
+
+	BYTE*	m_pEndpoint2ReportBuffer;
+	DWORD	m_Endpoint2ReportBufferSize;
+
+	FMRADIO_REGISTER	m_Register[FMRADIO_REGISTER_NUM];
+	SCRATCH_PAGE		m_ScratchPage;
+
+	CRDSData	m_RDS;
+	WORD		m_OldRegister;
+	WORD		m_OldRDSRegister[4];
+	bool		m_RDSCleared;
+	bool		primaryRadio;
+	
+	double	CalculateStationFrequency(FMRADIO_REGISTER hexChannel);
+	double  CalculateStationFrequency(FMRADIO_REGISTER hexChannel, BYTE ScratchBand, BYTE ScratchSpacing);
+	WORD	CalculateStationFrequencyBits(double frequency);
+
+	bool	SetScratchReport(BYTE report, BYTE* dataBuffer, DWORD dataBufferSize);
+	bool	GetScratchReport(BYTE report, BYTE* dataBuffer, DWORD dataBufferSize);
+	bool	SetLEDReport(BYTE report, BYTE* dataBuffer, DWORD dataBufferSize);
+	bool	SetStreamReport(BYTE report, BYTE* dataBuffer, DWORD dataBufferSize);
+
+public:
+	bool	pubSetRadioData(RadioData* radioData)
+	{
+		if(m_FMRadioDataHandle)
+			return(SetRadioData(radioData));
+		else
+			return(false);
+	}
+
+	bool	pubGetRadioData(RadioData* radioData)
+	{
+		if(m_FMRadioDataHandle)
+			return(GetRadioData(radioData));
+		else
+			return(false);
+	}
+	
+
+////////////////////////
+////////////////////////
+
+//////////////////////////
+//USB Audio  Functionality
+
+public:
+	bool	IsStreaming();
+	bool	IsTuning();
+	BYTE	GetWaveOutVolume();
+	bool	SetWaveOutVolume(BYTE level);
+
+	bool CreateRadioTimer();
+    bool DestroyRadioTimer();
+
+private:
+	bool	OpenFMRadioAudio();
+	bool	OpenSoundCard();
+	bool	CloseFMRadioAudio();	
+	bool	CloseSoundCard();
+	
+
+public:	
+	int CurrFreq;	
+	int QueFreq;
+	int QuePI;
+	int BackupFreq;
+	bool PopOut;
+	bool	ChangeLED(BYTE ledState);
+
+	bool TunerFound;
+
+	bool m_EONTAStart;
+	bool m_EONTAStop;
+	WORD m_EONTAPIStart;
+	WORD m_EONTAPIStop;
+	bool m_TrafficStartFlag;
+	bool m_TrafficEndFlag;
+
+	int m_StereoCount;	
+	bool m_isStereo;
+
+private:
+
+	bool		m_StreamingAllowed;
+	bool		m_AudioAllowed;
+	bool		m_Streaming;
+	bool		m_Tuning;
+	int			m_CurrentBlock;
+	int			m_FreeBlock;
+	
+	bool	StreamAudioIn();
+	bool	StreamAudioOut();
+
+	int     m_LastKnownRadioIndex;
+
+	IGraphBuilder*	g_pGraphBuilder;
+	ICaptureGraphBuilder2*  g_pCaptureGraphBuilder;
+	//CComPtr<IGraphBuilder> g_pGraphBuilder;
+
+	IMediaControl*	g_pMediaControl;
+	IMediaEventEx*	g_pMediaEvent;
+	IMediaPosition*	g_pMediaPosition;
+
+
+
+	HANDLE h_radioTimer;
+	HANDLE h_rdsTimer;
+
+
+////////////////////////////
+////////////////////////////
+};
+
+#endif // !defined(AFX_FMRADIODEVICE_H__6F0D8129_C34F_4CBC_93A0_EC3F2F68A8D1__INCLUDED_)
+
